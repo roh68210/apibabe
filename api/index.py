3156e6df5e10cb87951dde293b493e5a43148c6e
roh68210 <roh4127@gmail.com>
2026-04-10 12:51:49 +0530

    Update and rename drm.py to index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "API chal rahi hai 😎"}
+
 @app.get("/api")
 def get_data(url: str, token: str, auth: str):
     return {
